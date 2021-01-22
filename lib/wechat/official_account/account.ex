24b74ac10d987252b3db25e803229c06d9a541c6
feng19 <feng_19@foxmail.com>
2021-01-22 10:56:21 +0800

    feat: add WeChat.Account.get_auto_reply_rules/1

--- lib/wechat/official_account/account.ex
+++ lib/wechat/official_account/account.ex
@@ -124,4 +124,26 @@ defmodule WeChat.Account do
       query: [access_token: client.get_access_token()]
     )
   end
+
+  @doc """
+  获取公众号的自动回复规则 -
+  [Official API Docs Link](#{doc_link_prefix()}/doc/offiaccount/Message_Management/Getting_Rules_for_Auto_Replies.html){:target="_blank"}
+
+  获取公众号当前使用的自动回复规则，包括关注后自动回复、消息自动回复（60分钟内触发一次）、关键词自动回复。
+
+  请注意：
+
+  - 第三方平台开发者可以通过本接口，在旗下公众号将业务授权给你后，立即通过本接口检测公众号的自动回复配置。
+  - 本接口仅能获取公众号在公众平台官网的自动回复功能中设置的自动回复规则，若公众号自行开发实现自动回复，或通过第三方平台开发者来实现，则无法获取。
+  - 认证/未认证的服务号/订阅号，以及接口测试号，均拥有该接口权限。
+  - 从第三方平台的公众号登录授权机制上来说，该接口从属于消息与菜单权限集。
+  - 本接口中返回的图片/语音/视频为临时素材（临时素材每次获取都不同，3天内有效，通过素材管理-获取临时素材接口来获取这些素材），
+  本接口返回的图文消息为永久素材素材（通过素材管理-获取永久素材接口来获取这些素材）。
+  """
+  @spec get_auto_reply_rules(WeChat.client()) :: WeChat.response()
+  def get_auto_reply_rules(client) do
+    client.get("/cgi-bin/get_current_autoreply_info",
+      query: [access_token: client.get_access_token()]
+    )
+  end
 end
