8e2d2ba7fd7d50703beae2eaefe0b01fba9d8a0f
feng19 <feng_19@foxmail.com>
2021-06-29 14:32:06 +0800

    docs: add doc for WeChat pay

--- mix.exs
+++ mix.exs
@@ -2,12 +2,12 @@ defmodule WeChat.MixProject do
   use Mix.Project
   alias WeChat.{Requester, Refresher, ServerMessage, Storage, MiniProgram, Work}
 
-  @version "0.6.6"
+  @version "0.7.0"
 
   def project do
     [
       app: :wechat,
-      description: "WeChat SDK for Elixir, 支持: 公众号/小程序/第三方应用/企业微信",
+      description: "WeChat SDK for Elixir, 支持: 公众号/小程序/第三方应用/企业微信/微信支付",
       version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
