ffc37545def7b3d455945d34418226e8469eeaee
feng19 <kevin@feng19.com>
2023-10-11 18:17:41 +0800

    feat: add request_payment_args

--- lib/wechat/pay/transactions.ex
+++ lib/wechat/pay/transactions.ex
@@ -23,6 +23,25 @@ defmodule WeChat.Pay.Transactions do
     client.post("/v3/pay/transactions/jsapi", body)
   end
 
+  def request_payment_args(client, appid, prepay_id) do
+    timestamp = WeChat.Utils.now_unix() |> to_string()
+    nonce_str = :crypto.strong_rand_bytes(24) |> Base.encode64()
+    package = "prepay_id=#{prepay_id}"
+
+    sign =
+      "#{appid}\n#{timestamp}\n#{nonce_str}\n#{package}\n"
+      |> :public_key.sign(:sha256, client.private_key())
+      |> Base.encode64()
+
+    %{
+      "timeStamp" => timestamp,
+      "nonceStr" => nonce_str,
+      "package" => package,
+      "signType" => "RSA",
+      "paySign" => sign
+    }
+  end
+
   def query_by_out_trade_no(client, out_trade_no) do
     client.get(
       "/v3/pay/transactions/out-trade-no/#{out_trade_no}",
