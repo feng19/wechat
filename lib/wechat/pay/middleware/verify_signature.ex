5a8af789b895a3ed94fedad511c966c78d8709c7
feng19 <kevin@feng19.com>
2024-01-22 10:16:43 +0800

    enhance: decode error return for Pay.Middleware.VerifySignature

--- lib/wechat/pay/middleware/verify_signature.ex
+++ lib/wechat/pay/middleware/verify_signature.ex
@@ -14,17 +14,24 @@ defmodule WeChat.Pay.Middleware.VerifySignature do
 
   @impl Tesla.Middleware
   def call(env, next, client) do
-    with {:ok, %{status: 200, body: body} = env} when is_binary(body) <- Tesla.run(env, next) do
-      with nonce when is_binary(nonce) <- Tesla.get_header(env, "wechatpay-nonce"),
-           signature when is_binary(signature) <- Tesla.get_header(env, "wechatpay-signature"),
-           timestamp when is_binary(timestamp) <- Tesla.get_header(env, "wechatpay-timestamp"),
-           serial_no when is_binary(serial_no) <- Tesla.get_header(env, "wechatpay-serial"),
-           public_key when not is_nil(public_key) <- Certificates.get_cert(client, serial_no),
-           true <- Crypto.verify(signature, timestamp, nonce, body, public_key) do
+    case Tesla.run(env, next) do
+      {:ok, %{status: 200, body: body} = env} when is_binary(body) ->
+        with nonce when is_binary(nonce) <- Tesla.get_header(env, "wechatpay-nonce"),
+             signature when is_binary(signature) <- Tesla.get_header(env, "wechatpay-signature"),
+             timestamp when is_binary(timestamp) <- Tesla.get_header(env, "wechatpay-timestamp"),
+             serial_no when is_binary(serial_no) <- Tesla.get_header(env, "wechatpay-serial"),
+             public_key when not is_nil(public_key) <- Certificates.get_cert(client, serial_no),
+             true <- Crypto.verify(signature, timestamp, nonce, body, public_key) do
+          Tesla.Middleware.JSON.decode(env, [])
+        else
+          _error -> {:error, :invaild_response}
+        end
+
+      {:ok, %{body: body} = env} when is_binary(body) ->
         Tesla.Middleware.JSON.decode(env, [])
-      else
-        _error -> {:error, :invaild_response}
-      end
+
+      error ->
+        error
     end
   end
 end
